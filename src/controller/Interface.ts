2938dec1caba1a6905ad47c3d096264a8aa21a05
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-04-05 19:17:44 -0300

    Updated to private router and created a method to get the router

--- src/controller/Interface.ts
+++ src/controller/Interface.ts
@@ -1,8 +1,8 @@
 export interface Controller {
-    init_routes(): any;
     get_read(req: any, res: any): any;
     get_readByID(req: any, res: any): any;
     post_create(req: any, res: any): any;
     put_update(req: any, res: any): any;
     delete_delete(req: any, res: any): any;
+    get_router(): any;
 };
--- src/controller/ReportExpressController.ts
+++ src/controller/ReportExpressController.ts
@@ -5,21 +5,25 @@ import { ReportService } from "../service/ReportService";
 export class ReportExpressController implements Controller {
 
     private readonly service: ReportService = new ReportService();
-    public router: Router;
-    public path: string = '/report';
+    private router: Router;
+    private path: string = '/report';
 
     constructor(){
         this.router = Router();
         this.init_routes();
     };
 
-    public init_routes() {
+    private init_routes() {
         this.router.get(this.path, this.get_read.bind(this));
         this.router.get(this.path + '/:id', this.get_readByID.bind(this));
         this.router.post(this.path, this.post_create.bind(this));
         this.router.put(this.path + '/:id', this.put_update.bind(this));
         this.router.delete(this.path + '/:id', this.delete_delete.bind(this));
     };
+
+    public get_router() {
+        return this.router;    
+    }
 
     public async get_read(req: Request, res: Response){
         const response = await this.service.read();
