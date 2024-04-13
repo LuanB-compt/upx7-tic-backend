382ac714d98b0de92d63a02174da346597cf09ca
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-04-13 19:07:19 -0300

    Updated [main]: Public Servant

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import Application from "./src/app/Application";
 import { ReportController } from "./src/controller/ReportController";
 import { CityController } from "./src/controller/CityController";
+import { ServantController } from "./src/controller/ServantController";
 
-const app = new Application([new ReportController(), new CityController()], 3000);
+const app = new Application([new ReportController(), new CityController(), new ServantController()], 3000);
 app.listen();
