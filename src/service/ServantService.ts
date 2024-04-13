0704e4c0573c78b334d5ec27b2775f195774cf5d
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-04-13 19:06:18 -0300

    Created [service]: Public Servant

---
+++ src/service/ServantService.ts
@@ -0,0 +1,30 @@
+import { ServantRepository } from "../repository/ServantRepository";
+import { Service } from "./interface/Interface";
+
+export class ServantService implements Service {
+
+    private readonly repository: ServantRepository = new ServantRepository();
+
+    constructor(){};
+
+    public async read(): Promise<any | undefined> {
+        return await this.repository.read();
+    };
+
+    public async readByID(id: number): Promise<any | undefined> {
+        return await this.repository.readByID(id);
+    };
+
+    public async create(data: any): Promise<any | undefined> {
+        return await this.repository.create(data);
+    };
+
+    public async update(id: number, data: any): Promise<any | undefined> {
+        return await this.repository.update(id, data);
+    };
+
+    public async delete(id: number): Promise<any | undefined> {
+        return await this.repository.delete(id);
+    };
+
+};
