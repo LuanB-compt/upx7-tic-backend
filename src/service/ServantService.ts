99057b50be44f4b1f8e923595d710e7d913577e6
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-04-14 14:05:56 -0300

    Created [Public Servant]: signin and signup feature

--- src/service/ServantService.ts
+++ src/service/ServantService.ts
@@ -1,9 +1,11 @@
+import { ServantMiddleware } from "../middleware/ServantMiddleware";
 import { ServantRepository } from "../repository/ServantRepository";
 import { Public_Servant } from "../entity/Public_Servant";
 import { Service } from "./interface/Interface";
 
 export class ServantService implements Service {
 
+    private readonly middleware: ServantMiddleware = new ServantMiddleware();
     private readonly repository: ServantRepository = new ServantRepository();
 
     constructor(){};
@@ -26,6 +28,27 @@ export class ServantService implements Service {
 
     public async delete(id: number): Promise<number | undefined> {
         return await this.repository.delete(id);
+    };
+
+    public async signup(data: any): Promise<number | undefined> {
+        var response = await this.repository.readByFunctionalID(data.functional_identity);
+        if (response == undefined){
+            return undefined;
+        };
+        data.passowrd = this.middleware.passwd_hash(data.passowrd);
+        return await this.repository.create(data);
+    };
+
+    public async signin(functional_identity: string, passwd: string): Promise<boolean | undefined> {
+        let response = await this.repository.readByFunctionalID(functional_identity);
+        if (response == undefined){
+            return undefined;
+        };
+        let check = await this.middleware.passwd_compare(passwd, response.passowrd);
+        if(!check){
+            return false;
+        }
+        return true;
     };
 
 };
