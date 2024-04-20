1adee8abb2437f1198b0377d23d46667f1f6bbd2
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-04-20 13:48:49 -0300

    Created Auth middleware

---
+++ src/middleware/dependencies/AuthMiddlewareExpress.ts
@@ -0,0 +1,32 @@
+import bcrypt from "bcrypt";
+import { Request, Response, NextFunction } from "express";
+import { AuthService } from "../../service/AuthService";
+import { ServantService } from "../../service/ServantService";
+
+export class AuthMiddlewareExpress {
+
+    private salt_or_rounds: number = 10;
+    private readonly service: AuthService = new AuthService();
+    private readonly servant: ServantService = new ServantService();
+
+    public async passwd_hash(password: string): Promise<string>{
+        return await bcrypt.hash(password, this.salt_or_rounds);
+    };
+
+    public async passwd_compare(to_check: string, to_compare: string): Promise<boolean> {
+        return await bcrypt.compare(to_check, to_compare);
+    };
+
+    public async verify_jwt(req: Request, res: Response, next: NextFunction) {
+        const token = req.header('Authorization');
+        if (token == undefined) {
+            res.status(401).json({message:"Without authorization"});
+        } else {
+            const response = await this.service.verifyByUser(token);
+            if (response == false) {
+                res.status(406).json({message:"Not authorized"});
+            } else {next()};
+        };
+    };
+
+}
