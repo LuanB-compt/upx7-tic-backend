4970d5f69a6e31a22a9ecccca8787efd3cef84e3
LuanB-compt <luan-bruno-domingues@hotmail.com>
2024-05-12 12:03:01 -0300

    Fixed name endpoints to better organization

--- src/controller/dependencies/ReportControllerExpress.ts
+++ src/controller/dependencies/ReportControllerExpress.ts
@@ -16,8 +16,8 @@ export class ReportControllerExpress implements Controller {
     private init_routes() {
         this.router.get(this.path, this.get_read.bind(this));
         this.router.get(this.path + '/:id', this.get_readByID.bind(this));
-        this.router.get(this.path + '/close/:city', this.get_close_reports.bind(this));
-        this.router.get(this.path + '/open/:city', this.get_open_reports.bind(this));
+        this.router.get(this.path + '/close/:city', this.get_close_city.bind(this));
+        this.router.get(this.path + '/open/:city', this.get_open_city.bind(this));
     
         this.router.post(this.path, this.post_create.bind(this));
     };
@@ -44,7 +44,7 @@ export class ReportControllerExpress implements Controller {
         };
     };
 
-    public async get_close_reports(req: Request, res: Response){
+    public async get_close_city(req: Request, res: Response){
         const response = await this.service.read_by_city(req.params.city, true);
         if (response != undefined) {
             res.status(200).json(response);
@@ -53,7 +53,7 @@ export class ReportControllerExpress implements Controller {
         };
     };
 
-    public async get_open_reports(req: Request, res: Response){
+    public async get_open_city(req: Request, res: Response){
         const response = await this.service.read_by_city(req.params.city, false);
         if (response != undefined) {
             res.status(200).json(response);
@@ -61,6 +61,7 @@ export class ReportControllerExpress implements Controller {
             res.status(400).json({message:"error"});
         };
     };
+
 
     public async post_create(req: Request, res: Response){
         const response = await this.service.create(req.body);
