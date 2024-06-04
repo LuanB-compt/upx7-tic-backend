aa228d5e502ca7adda31232fa78bbe56f8ef80cf
Nathan Roberto <nathan.rgs@gmail.com>
2024-06-03 23:54:46 -0300

    feat: adding debugger cors and img visualize route

--- src/app/ApplicationExpress.ts
+++ src/app/ApplicationExpress.ts
@@ -1,33 +1,36 @@
+import cors from "cors";
 import express from "express";
-import { Controller } from "../interface/controller/Interface";
 import { AppInterface } from "../interface/app/Interface";
+import { Controller } from "../interface/controller/Interface";
 
 export class ApplicationExpress implements AppInterface {
+  private app: express.Application;
+  private port: number;
 
-    private app: express.Application;
-    private port: number;
+  constructor(controllers: Controller[], port: number = 3000) {
+    this.app = express();
+    this.port = port;
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+  }
 
-    constructor(controllers: Controller[], port: number = 3000){
-        this.app = express();
-        this.port = port;
-        this.initializeMiddlewares();
-        this.initializeControllers(controllers);
-    };
+  private initializeMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
 
-    private initializeMiddlewares() {
-        this.app.use(express.json());
-        this.app.use(express.urlencoded({extended:true}));
-    };
+    // Habilita o CORS para todas as origens
+    this.app.use(cors());
+  }
 
-    private initializeControllers(controllers: Controller[]){
-        controllers.forEach((controller: Controller) => {
-            this.app.use('/', controller.get_router());
-        });
-    };
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach((controller: Controller) => {
+      this.app.use("/", controller.get_router());
+    });
+  }
 
-    public listen() {
-        this.app.listen(this.port, () => {
-            console.log("Running on port " + this.port);
-        });
-    };
-};
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log("Running on port " + this.port);
+    });
+  }
+}
